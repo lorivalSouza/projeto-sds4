82584e81926295fa68fb46b5532586995aca0521
lorivalSouza <lorivalnew@gmail.com>
2021-09-11 15:41:03 -0300

    Pagination

--- frontend/src/pages/Dashboard/index.tsx
+++ frontend/src/pages/Dashboard/index.tsx
@@ -1,4 +1,4 @@
-import NavBar from "components";
+import NavBar from "components/NavBar";
 import BarChart from "components/BarChart";
 import DataTable from "components/DataTable";
 import DonutChart from "components/DonutChart";
--- frontend/src/pages/Home/index.tsx
+++ frontend/src/pages/Home/index.tsx
@@ -1,4 +1,4 @@
-import NavBar from "components";
+import NavBar from "components/NavBar";
 import Footer from "components/Footer";
 import { Link } from "react-router-dom";
 
